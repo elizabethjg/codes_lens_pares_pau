d7843a294554c6b8454f89abfc28e35a2c194f83
elizabethjg <elizabet.johana.gonzalez@gmail.com>
2022-02-08 12:32:33 -0300

    update

--- run_fit.sh
+++ run_fit.sh
@@ -4,8 +4,6 @@ source /home/elizabeth/.bashrc
 
 conda activate myenv
 
-python -u fit_pairs_profile.py -ncores 15 -folder ~/PARES-PAU/profiles/ -file 'profile_all_100_10000_photo_z_2nd_run_mag_i_mask.fits'  -RIN 200 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 15 -folder ~/PARES-PAU/profiles/ -file 'profile_all_100_10000_zspec_mask.fits'  -RIN 200 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 15 -folder ~/PARES-PAU/profiles/ -file 'profile_all_200_10000_photo_z_2nd_run_mag_i_mask.fits'  -RIN 200 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 15 -folder ~/PARES-PAU/profiles/ -file 'profile_all_200_10000_zspec_mask.fits' -RIN 200 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 15 -folder ~/PARES-PAU/profiles/ -file 'profile_all_100_10000_zback_photo_z_2nd_run_mag_i_mask.fits'  -RIN 200 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 15 -folder ~/PARES-PAU/profiles/ -file 'profile_all_100_10000_zback_zspec_mask.fits'  -RIN 200 -ROUT 10000 &
 wait
