899f54b2fd56d3ccd7c31cd1e6e1a9d0f218e6dc
elizabethjg <elizabet.johana.gonzalez@gmail.com>
2022-02-16 08:59:22 -0300

    update

--- analysis_profile.py
+++ analysis_profile.py
@@ -87,6 +87,81 @@ def plt_profile_wofit(samp):
     f.savefig(folder+'plots/profile_'+samp+'.png',bbox_inches='tight')
     
 
+def plt_profile_align(samp):
+    
+
+    p_name = 'profile_'+samp+'.fits'
+    profile = fits.open(folder+p_name)
+
+    print(p_name)
+    
+    # '''
+    h   = profile[1].header
+    p   = profile[1].data
+    
+    cosmo_as = LambdaCDM(H0=100, Om0=0.3, Ode0=0.7)
+    '''
+    
+    h = profile[1].header
+    p = profile[1].data
+    '''
+
+    ### compute dilution
+    bines = np.logspace(np.log10(h['RIN']),np.log10(h['ROUT']),num=len(p)+1)
+    area = np.pi*np.diff(bines**2)
+    
+    ngal = p.NGAL_w
+    
+    d = ngal/area
+    
+    fcl = ((d - np.mean(d[-2:]))*area)/ngal
+    
+    bcorr = 1./(1-fcl)
+
+    p.DSigma_T = bcorr*p.DSigma_T
+    p.DSigma_X = bcorr*p.DSigma_X
+    p.error_DSigma_T = bcorr*p.error_DSigma_T
+    p.error_DSigma_X = bcorr*p.error_DSigma_X
+    
+    zmean = h['z_mean']    
+    
+    ndots = p.shape[0]
+    
+    
+    # FIT MONOPOLE
+    nfw     = Delta_Sigma_fit(p.Rp,p.DSigma_T,p.error_DSigma_T,zmean,cosmo_as)
+    
+    
+    mass = str(np.round(np.log10(nfw.M200),2))
+    cfit = str(np.round((nfw.c200),2))
+    
+    f, ax = plt.subplots(2, 1, figsize=(6,6),sharex = True)
+    f.subplots_adjust(hspace=0)
+                    
+    ax[0].plot(p.Rp,p.DSigma_T,'C1o')
+    ax[0].errorbar(p.Rp,p.DSigma_T,yerr=p.error_DSigma_T,ecolor='C1',fmt='None')
+    ax[0].plot(nfw.xplot,nfw.yplot,'C3',label='fitted nfw $\log M_{200}=$'+mass+' $c_{200} = $'+cfit)
+    ax[0].fill_between(p.Rp,p.DSigma_T+p.error_DSigma_T,p.DSigma_T-p.error_DSigma_T,color='C1',alpha=0.3)
+    ax[0].set_xscale('log')
+    # ax[0].set_yscale('log')
+    ax[0].set_ylabel(r'$\Delta\Sigma_{T} [M_{\odot}pc^{-2} h ]$')
+    ax[0].set_ylim(-40,40)
+    ax[0].set_xlim(h['RIN']/1000.,h['ROUT']/1000.)
+    # ax[0].yaxis.set_ticks([0.1,1,10,100])
+    # ax[0].set_yticklabels([0.1,1,10,100])
+    ax[0].legend()
+
+    ax[1].plot(p.Rp,p.DSigma_X,'C7x')
+    ax[1].errorbar(p.Rp,p.DSigma_X,yerr=p.error_DSigma_X,ecolor='C7',fmt='None')
+    ax[1].fill_between(p.Rp,p.DSigma_X+p.error_DSigma_X,p.DSigma_X-p.error_DSigma_X,color='C7',alpha=0.3)
+    ax[1].set_ylabel(r'$\Delta\Sigma_{\times} [M_{\odot}pc^{-2} h ]$')
+    ax[1].set_xlabel(r'$R [Mpc/h]$')
+    ax[1].plot([0,h['ROUT']/1000.],[0,0],'k--')
+    ax[1].set_ylim(-40,40)
+    
+    f.savefig(folder+'plots/profile_'+samp+'.png',bbox_inches='tight')
+
+
 def plt_profile_fit_2h(samp,RIN,ROUT):
     
 
