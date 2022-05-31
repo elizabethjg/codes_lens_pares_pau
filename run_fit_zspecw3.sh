408700fb526bd1b2ac533cb96bb175579cedd7f2
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2022-05-31 11:34:07 -0300

    run alumnos

--- run_fit_zspecw3.sh
+++ run_fit_zspecw3.sh
@@ -4,14 +4,14 @@ source /home/elizabeth/.bashrc
 
 conda activate myenv
 
-python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles/ -file 'profile_wc_all__zspec_best.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles/ -file 'profile_wc_LrM_all__zspec_best.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles/ -file 'profile_wc_Lrm_all__zspec_best.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles/ -file 'profile_wc_zM_all__zspec_best.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles/ -file 'profile_wc_zm_all__zspec_best.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles/ -file 'profile_wc_all__zspec.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles/ -file 'profile_wc_LrM_all__zspec.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles/ -file 'profile_wc_Lrm_all__zspec.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles/ -file 'profile_wc_zM_all__zspec.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles/ -file 'profile_wc_zm_all__zspec.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles_new/ -file 'profile_wc_all__zspec_best.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles_new/ -file 'profile_wc_LrM_all__zspec_best.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles_new/ -file 'profile_wc_Lrm_all__zspec_best.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles_new/ -file 'profile_wc_zM_all__zspec_best.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles_new/ -file 'profile_wc_zm_all__zspec_best.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles_new/ -file 'profile_wc_all__zspec.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles_new/ -file 'profile_wc_LrM_all__zspec.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles_new/ -file 'profile_wc_Lrm_all__zspec.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles_new/ -file 'profile_wc_zM_all__zspec.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile_withcov.py -ncores 4 -folder ~/PARES-PAU/profiles_new/ -file 'profile_wc_zm_all__zspec.fits'  -RIN 300 -ROUT 10000 &
 wait
