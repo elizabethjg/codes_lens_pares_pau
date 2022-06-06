793c26aa5545fea5b034b3fc826869e4ed52269d
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2022-06-06 09:26:15 -0300

    update

--- analysis_profile.py
+++ analysis_profile.py
@@ -166,7 +166,9 @@ def plt_profile_align(samp):
     f.savefig(folder+'plots/profile_'+samp+'.png',bbox_inches='tight')
 
 
-def plt_profile_fit_2h(samp,RIN,ROUT):
+def plt_profile_fit_2h(samp,lsamp,
+                       axDS,axC,
+                       RIN=300,ROUT=10000):
     
 
     p_name = 'profile_'+samp+'.fits'
@@ -175,15 +177,17 @@ def plt_profile_fit_2h(samp,RIN,ROUT):
     print(p_name)
     
     # '''
-    h   = profile[1].header
+    h   = profile[0].header
     p   = profile[1].data
+    cov = profile[2].data
+
+    CovDST  = cov.COV_ST.reshape(len(p),len(p))
+    CovDSX  = cov.COV_SX.reshape(len(p),len(p))
     
     cosmo_as = LambdaCDM(H0=100, Om0=0.3, Ode0=0.7)
-    '''
-    
-    h = profile[1].header
-    p = profile[1].data
-    '''
+
+    error_DSX = np.sqrt(np.diag(CovDSX))
+    error_DST = np.sqrt(np.diag(CovDST))
 
     ### compute dilution
     bines = np.logspace(np.log10(h['RIN']),np.log10(h['ROUT']),num=len(p)+1)
@@ -216,48 +220,40 @@ def plt_profile_fit_2h(samp,RIN,ROUT):
     rplot = np.logspace(np.log10(h['RIN']/1000.),np.log10(h['ROUT']/1000.),20)
     
     nfw  = Delta_Sigma_fit(p.Rp,p.DSigma_T,p.error_DSigma_T,zmean,cosmo_as)
-    ds2h   = Delta_Sigma_NFW_2h(rplot,zmean,M200 = 10**fitpar['lM200'],c200=fitpar['c200'],cosmo_params=params,terms='2h')    
+    # ds2h   = Delta_Sigma_NFW_2h(rplot,zmean,M200 = 10**fitpar['lM200'],c200=fitpar['c200'],cosmo_params=params,terms='2h')    
     ds1h   = Delta_Sigma_NFW_2h(rplot,zmean,M200 = 10**fitpar['lM200'],c200=fitpar['c200'],cosmo_params=params,terms='1h')    
     
-    ds = ds1h +ds2h
+    ds = ds1h #+ds2h
     
     mass = str(np.round(fitpar['lM200'],2))
     cfit = str(np.round(fitpar['c200'],2))
+                        
+    axDS.plot(p.Rp,p.DSigma_T,'C1o')
+    axDS.errorbar(p.Rp,p.DSigma_T,yerr=p.error_DSigma_T,ecolor='C1',fmt='None')
+    axDS.plot(rplot,ds,'C3',label=lsamp+' $\log M_{200}=$'+mass+' $c_{200} = $'+cfit)
+    axDS.plot(rplot,ds1h,'C4')
+    # axDS.plot(rplot,ds2h,'C4--')
+    axDS.fill_between(p.Rp,p.DSigma_T+error_DST,p.DSigma_T-error_DST,color='C1',alpha=0.4)
+    axDS.set_xscale('log')
+    axDS.set_yscale('log')
+    axDS.set_ylabel(r'$\Delta\Sigma_{T} [M_{\odot}pc^{-2} h ]$')
+    axDS.set_ylim(0.05,100)
+    axDS.set_xlim(h['RIN']/1000.,h['ROUT']/1000.)
+    axDS.yaxis.set_ticks([0.1,1,10,100])
+    axDS.set_yticklabels([0.1,1,10,100])
+    axDS.axvline(RIN/1000.,color='C7')
+    axDS.axvline(ROUT/1000.,color='C7')    
+    axDS.legend(frameon=False,loc=1)
     
-    f, ax = plt.subplots(2, 1, gridspec_kw={'height_ratios': [3, 1]},figsize=(6,6),sharex = True)
-    f.subplots_adjust(hspace=0)
-                    
-    ax[0].plot(p.Rp,p.DSigma_T,'C1o')
-    ax[0].errorbar(p.Rp,p.DSigma_T,yerr=p.error_DSigma_T,ecolor='C1',fmt='None')
-    ax[0].plot(rplot,ds,'C3',label='fitted nfw $\log M_{200}=$'+mass+' $c_{200} = $'+cfit)
-    ax[0].plot(rplot,ds1h,'C4')
-    ax[0].plot(rplot,ds2h,'C4--')
-    ax[0].fill_between(p.Rp,p.DSigma_T+p.error_DSigma_T,p.DSigma_T-p.error_DSigma_T,color='C1',alpha=0.4)
-    ax[0].set_xscale('log')
-    ax[0].set_yscale('log')
-    ax[0].set_ylabel(r'$\Delta\Sigma_{T} [M_{\odot}pc^{-2} h ]$')
-    ax[0].set_ylim(0.05,100)
-    ax[0].set_xlim(h['RIN']/1000.,h['ROUT']/1000.)
-    ax[0].yaxis.set_ticks([0.1,1,10,100])
-    ax[0].set_yticklabels([0.1,1,10,100])
-    ax[0].axvline(RIN/1000.,color='C7')
-    ax[0].axvline(ROUT/1000.,color='C7')    
-    ax[0].legend()
-
-    ax[1].plot(p.Rp,p.DSigma_X,'C7x')
-    ax[1].errorbar(p.Rp,p.DSigma_X,yerr=p.error_DSigma_X,ecolor='C7',fmt='None')
-    ax[1].fill_between(p.Rp,p.DSigma_X+p.error_DSigma_X,p.DSigma_X-p.error_DSigma_X,color='C7',alpha=0.4)
-    ax[1].set_ylabel(r'$\Delta\Sigma_{\times} [M_{\odot}pc^{-2} h ]$')
-    ax[1].set_xlabel(r'$R [Mpc/h]$')
-    ax[1].plot([h['RIN']/1000.,h['ROUT']/1000.],[0,0],'k--')
-    ax[1].set_ylim(-7,7)
     
-    f.savefig(folder+'plots/profile_'+samp+'_2h.png',bbox_inches='tight')
+    axC.plot(lgM,label=lsamp,alpha=0.5)
+    axC.axhline(np.median(lgM[2500:]),alpha=0.5)
+    axC.axhline(np.percentile(lgM[2500:], [16,50,84])[0],ls='--')
+    axC.axhline(np.percentile(lgM[2500:], [16,50,84])[2],ls='--')
+    axC.axvline(2500)
+    axC.legend(frameon=False,loc=1)
     
-    plt.figure()
-    plt.plot(lgM,alpha=0.5)
-    plt.axvline(2500)
-    plt.savefig(folder+'plots/fit_'+samp+'_'+str(RIN)+'_'+str(ROUT)+'.png',bbox_inches='tight')
+    return np.percentile(lgM[2500:], [16,50,84])
 
 
 def dilution(samp):
@@ -303,7 +299,40 @@ def test():
     plt.xscale('log')
     plt.xlabel('R [Mpc]')
     plt.ylabel('n')
-    
+
+
+fDS, axDS = plt.subplots(5,2, figsize=(12,14),sharex = True,sharey = True)
+fDS.subplots_adjust(hspace=0,wspace=0)
+
+fC, axC = plt.subplots(5,2, figsize=(12,14),sharex = True,sharey = True)
+fC.subplots_adjust(hspace=0,wspace=0)
+
+axDS = axDS.flatten()
+axC = axC.flatten()
+
+pcat = '_zspec_best'
+
+samp =  ['wc_all_'+pcat,'wc_w3_'+pcat,
+         'wc_LrM_all_'+pcat,'wc_LrM_w3_'+pcat,
+         'wc_Lrm_all_'+pcat,'wc_Lrm_w3_'+pcat,
+         'wc_zM_all_'+pcat,'wc_zM_w3_'+pcat,
+         'wc_zm_all_'+pcat,'wc_zm_w3_'+pcat]
+
+lsamp = ['all -','',
+         'HLratio -','',
+         'LLratio -','',
+         'Hz -','',
+         'Lz -','']
+
+for j in range(len(axDS)):
+    plt_profile_fit_2h(samp[j],lsamp[j],axDS[j],axC[j])
+
+
+fDS.savefig('../profile'+pcat+'.png',bbox_inches='tight')
+fC.savefig('../chains'+pcat+'.png',bbox_inches='tight')
+
+
+
 '''
 plt.figure()    
 plt.plot(w1_sources.RAJ2000,w1_sources.DECJ2000,',')
