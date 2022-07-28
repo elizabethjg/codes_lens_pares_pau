a4d6166812c3ee6a383169a1dea758d43af96425
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2022-07-28 12:51:36 -0300

    update

--- run_fit_zphotw3.sh
+++ run_fit_zphotw3.sh
@@ -9,9 +9,9 @@ python -u fit_pairs_profile.py -ncores 5 -folder ~/PARES-PAU/profiles3/ -file 'p
 python -u fit_pairs_profile.py -ncores 5 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_Lrm_all__photo_z_2nd_run_mag_i_best.fits'  -RIN 300 -ROUT 10000 &
 python -u fit_pairs_profile.py -ncores 5 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_zm_all__photo_z_2nd_run_mag_i_best.fits'  -RIN 300 -ROUT 10000 &
 python -u fit_pairs_profile.py -ncores 5 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_zM_all__photo_z_2nd_run_mag_i_best.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_MM_all__photo_z_2nd_run_mag_i_best.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_Mm_all__photo_z_2nd_run_mag_i_best.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_red_all__photo_z_2nd_run_mag_i_best.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 5 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_MM_all__photo_z_2nd_run_mag_i_best.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 5 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_Mm_all__photo_z_2nd_run_mag_i_best.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 5 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_red_all__photo_z_2nd_run_mag_i_best.fits'  -RIN 300 -ROUT 10000 &
 python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_blue_all__photo_z_2nd_run_mag_i_best.fits'  -RIN 300 -ROUT 10000 &
 python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_M1_all__photo_z_2nd_run_mag_i_best.fits'  -RIN 300 -ROUT 10000 &
 python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_M2_all__photo_z_2nd_run_mag_i_best.fits'  -RIN 300 -ROUT 10000 &
