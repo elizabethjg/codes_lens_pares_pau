b1a9d4f9e5c77fc77376cdfa91dbf5a1865c9e7d
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2022-09-02 16:24:12 -0300

    update

--- run_fit_zphot.sh
+++ run_fit_zphot.sh
@@ -4,17 +4,17 @@ source /home/elizabeth/.bashrc
 
 conda activate myenv
 
-##python -u fit_pairs_profile.py -ncores 5 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_all__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 15 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_LrM2_all__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 15 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_Lrm2_all__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 5 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_zm_all__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 5 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_zM_all__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_MM_all__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_Mm_all__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_red_all__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_blue_all__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_M1_all__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_M2_all__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_M3_all__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_M4_all__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_w1w3__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_LrM2_w1w3__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_Lrm2_w1w3__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_zm_w1w3__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_zM_w1w3__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_MM_w1w3__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_Mm_w1w3__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_red_w1w3__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_blue_w1w3__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
+##python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_M1_w1w3__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
+##python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_M2_w1w3__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
+##python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_M3_w1w3__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
+##python -u fit_pairs_profile.py -ncores 4 -folder ~/PARES-PAU/profiles3/ -file 'profile_mh_M4_w1w3__photo_z_2nd_run_mag_i.fits'  -RIN 300 -ROUT 10000 &
 wait
