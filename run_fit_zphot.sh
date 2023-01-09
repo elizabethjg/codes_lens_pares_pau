1618b4d175c97a4e922dca0ab3e16985fb3142bb
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2023-01-09 14:41:25 -0300

    update

--- run_fit_zphot.sh
+++ run_fit_zphot.sh
@@ -4,13 +4,22 @@ source /home/elizabeth/.bashrc
 
 conda activate myenv
 
-python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_LrM2_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_Lrm2_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_zm_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_zM_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_MM_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_Mm_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_red_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_blue_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
+##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_LrM2_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_Lrm2_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_zm_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_zM_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_MM_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_Mm_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_red_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_blue_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_LrM2_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_Lrm2_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_zm_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_zM_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_MM_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_Mm_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_red_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_blue_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
 wait
