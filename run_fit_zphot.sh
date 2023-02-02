0ce58a725475da22a4eef3550aa1a008434d8aeb
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2023-02-02 10:11:34 +0100

    fit iso

--- run_fit_zphot.sh
+++ run_fit_zphot.sh
@@ -13,13 +13,8 @@ conda activate myenv
 ##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_Mm_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
 ##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_red_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
 ##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_blue_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_LrM2_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_Lrm2_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_zm_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_zM_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_MM2_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_Mm2_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_red_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_blue_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 30 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_radio7_w1w3_Total-7radios.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 30 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_radio7_w1w3_Gold-7radios.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 30 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_radio7_w1w3_Total-10radios.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 30 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_radio7_w1w3_Gold-10radios.fits'  -RIN 300 -ROUT 10000 &
 wait
