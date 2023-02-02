acd4e87f81d77da4d5cf7c851e954d6d3fcbe328
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2023-02-02 10:23:45 +0100

    fit iso

--- run_fit_zphot.sh
+++ run_fit_zphot.sh
@@ -15,6 +15,6 @@ conda activate myenv
 ##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_blue_w1_Vane.fits'  -RIN 300 -ROUT 10000 &
 python -u fit_pairs_profile.py -ncores 30 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_radio7_w1w3_Total-7radios.fits'  -RIN 300 -ROUT 10000 &
 python -u fit_pairs_profile.py -ncores 30 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_radio7_w1w3_Gold-7radios.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 30 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_radio7_w1w3_Total-10radios.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 30 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_radio7_w1w3_Gold-10radios.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 30 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_radio10_w1w3_Total-10radios.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 30 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_radio10_w1w3_Gold-10radios.fits'  -RIN 300 -ROUT 10000 &
 wait
