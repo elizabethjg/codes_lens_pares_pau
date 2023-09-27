d72ca0ffb8aa823dbf915acc791eb45e98ae9940
elizabethjg <elizabeth.johana.gonzalez@gmail.com>
2023-09-27 16:56:16 +0200

    run fit deepz

--- run_fit_zphot.sh
+++ run_fit_zphot.sh
@@ -4,13 +4,13 @@ source /home/elizabeth/.bashrc
 
 conda activate myenv
 
-##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_LrM2_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_Lrm2_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_zm_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_zM_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 15 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_MM2_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-python -u fit_pairs_profile.py -ncores 15 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_Mm2_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_red_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
-##python -u fit_pairs_profile.py -ncores 7 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_blue_w1w3_Total.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_w1w3_deepz.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_LrM2_w1w3_deepz.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_Lrm2_w1w3_deepz.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_zm_w1w3_deepz.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_zM_w1w3_deepz.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 10  -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_MM2_w1w3_deepz.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 10  -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_Mm2_w1w3_deepz.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_red_w1w3_deepz.fits'  -RIN 300 -ROUT 10000 &
+python -u fit_pairs_profile.py -ncores 10 -folder ~/PARES-PAU/profiles_pk/ -file 'profile_pk_blue_w1w3_deepz.fits'  -RIN 300 -ROUT 10000 &
 wait
